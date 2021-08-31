8f82fba11e1164059a596d31ffa07a9633eeea85
ryansereno <ryansereno@protonmail.com>
2021-08-31 16:46:06 -0700

    added recursive length function

--- recurssion.hs
+++ recurssion.hs
@@ -2,7 +2,13 @@ fib 0 = 0
 fib 1 = 1
 fib n = fib (n-1) + fib (n-2) 
 
-fact 1 = 1
+fact 0 = 1
 fact n = n * fact (n-1)
 
+head' :: [a] -> a
+head' [] = error "Can't call head on empty list"
+head' (x:_) = x
 
+length' :: (Num b) => [a] -> b
+length' [] = 0
+length' (_:xs) = 1 + length' xs
